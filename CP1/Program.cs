e5117e3d0558cdc074580ddcbc4c467bf6e1f3cb
Luis Alberto Rocha Filho <118944493+luisalbertorocha@users.noreply.github.com>
2025-09-08 10:28:16 -0300

    adcionando refatoracao da variant3

---
+++ CP1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Projeto de Avaliação (Revisado) - Variante 3: Cálculo de média de três notas
+// Este programa deve solicitar o nome do estudante e três notas, calcular e exibir a média.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite o nome do estudante:");
+
+        string nome = Console.ReadLine();
+        double soma = 0;
+
+        for (int i = 1; i <= 3; i++)
+        {
+            Console.WriteLine("Digite a nota " + i + ":");
+            double nota = Convert.ToDouble(Console.ReadLine());
+
+            soma = soma + nota;
+        }
+
+        double media = soma / 3;
+        Console.WriteLine("A média do estudante " + nome + " é " + media);
+    }
+}
